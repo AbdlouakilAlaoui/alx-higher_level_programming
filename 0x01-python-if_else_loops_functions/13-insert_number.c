ac2dc3ed0ddb25de75848e531a7886e6e212ab5a
AbdlouakilAlaoui <abdlouakilalaoui11@gmail.com>
2023-11-29 14:28:43 -0800

    abdo

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -9,29 +9,27 @@
  * Return: If the function fails - NULL.
  * Otherwise - a pointer to the new node.
  */
-listint_t *insert_node(listint_t **head, int number);
+listint_t *insert_node(listint_t **head, int number)
 {
-        listint_t *node = *head, * new = malloc(sizeof(listint_t));
-        if (!new)
+        listint_t *node = *head, *new;
+
+        new = malloc(sizeof(listint_t));
+        if (new == NULL)
                 return (NULL);
         new->n = number;
-	new->next = NULL;
 
-        if (!node || node->n < node->n)
+        if (node == NULL || node->n >= number)
         {
                 new->next = node;
-                return (*head = new);
+                *head = new;
+                return (new);
         }
 
-        while (node)
-	{
-		if (!node->next || new->n < node->next->n)
-		{
-                    new->next = node->next;
-                    node->next = new;
-                    return (node);
-		 }
-		node = node->next;
-	}
-	return(NULL)
+        while (node && node->next && node->next->n < number)
+                node = node->next;
+
+        new->next = node->next;
+        node->next = new;
+
+        return (new);
 }
