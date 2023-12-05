de529dafb35d2bdb023c731e170bbf67485e7ef4
AbdlouakilAlaoui <abdlouakilalaoui11@gmail.com>
2023-12-04 17:50:42 -0800

    abdo

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,44 +1,41 @@
-#include <lists.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-typedef struct listint {
-    int data;
+typedef struct listint 
+{
+    int n;
     struct listint *next;
 } listint_t;
 
-listint_t* reverseList(listint_t* head) {
-    listint_t *prev = NULL, *current = head, *next = NULL;
-    while (current != NULL) {
-        next = current->next;
-        current->next = prev;
-        prev = current;
-        current = next;
-    }
-    return prev;
-}
-
-int is_palindrome(listint_t **head)
-{
-    if (*head == NULL || (*head)->next == NULL){
-        return 1; // An empty list or a list with one element is a palindrome
+int is_palindrome(listint_t **head) {
+    if (*head == NULL || (*head)->next == NULL) {
+        return 1;
     }
 
-    listint_t *slow = *head, *fast = *head;
+    listint_t *slow = *head;
+    listint_t *fast = *head;
+    listint_t *prev = NULL;
+    listint_t *temp;
 
-    // Find the middle of the linked list
     while (fast != NULL && fast->next != NULL) {
-        slow = slow->next;
         fast = fast->next->next;
+        temp = slow->next;
+        slow->next = prev;
+        prev = slow;
+        slow = temp;
     }
- listint_t* reversedSecondHalf = reverseList(slow);
 
-    listint_t *firstHalf = *head, *secondHalf = reversedSecondHalf;
-    while (secondHalf != NULL) {
-        if (firstHalf->data != secondHalf->data) {
-            return 0; // Not a palindrome
+    if (fast != NULL) {
+        slow = slow->next;
+    }
+
+    while (slow != NULL) {
+        if (prev->n != slow->n) {
+            return 0; 
         }
-        firstHalf = firstHalf->next;
-        secondHalf = secondHalf->next;
+        prev = prev->next;
+        slow = slow->next;
     }
 
-    return 1;
+    return 1; 
 }
