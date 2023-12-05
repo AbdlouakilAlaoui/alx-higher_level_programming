ccb88627b44f1592be0dcdbda13e206ab54bbda2
AbdlouakilAlaoui <abdlouakilalaoui11@gmail.com>
2023-12-04 16:43:13 -0800

    abdo

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+
+
 def magic_calculation(a, b):
     """Match bytecode provided by Holberton School."""
     from magic_calculation_102 import add, sub
-        if a < b:
+
+    if a < b:
         c = add(a, b)
         for i in range(4, 6):
             c = add(c, i)
@@ -10,4 +13,3 @@ def magic_calculation(a, b):
 
     else:
         return(sub(a, b))
-    return 0
